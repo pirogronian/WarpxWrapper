1a61700337daff46b96362c6d7d891c2335aa6f3
Łukasz Buczyński <lukasz.a.buczynski@gmail.com>
2026-07-18 13:26:06 +0200

    Common param Action and its test usage.

--- WarpxWrapper.py
+++ WarpxWrapper.py
@@ -250,6 +250,16 @@ class IncludeAction(argparse.Action):
 
 parser.add_argument("-i", "--include", nargs='+', action=IncludeAction)
 
+class ParamAction(argparse.Action):
+    Param = ""
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        print("{}.__init__({}, {}, {}, {})".format(self.__class__.__name__, option_strings, dest, nargs, kwargs))
+        self.Param = kwargs.pop("VarName")
+        super().__init__(option_strings, dest, nargs = nargs, **kwargs)
+    def __call__(self, parser, namespace, value, option_string):
+        LogDebug("{}[{}] := {}".format(self.__class__.__name__, self.Param, value))
+parser.add_argument("-p", "--param", nargs='?', action=ParamAction, VarName="Właściwość", const="Domyślne")
+
 args = parser.parse_args(sys.argv[1:])
 LogDebug("Options from command line: {}".format(args))
 
