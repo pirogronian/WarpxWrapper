779762bf4209868b1292f0a2de79410a9621eb59
Łukasz Buczyński <lukasz.a.buczynski@gmail.com>
2026-08-01 19:27:17 +0200

    Expose StrToInt for include files.

--- WarpxWrapper/__init__.py
+++ WarpxWrapper/__init__.py
@@ -8,7 +8,7 @@ from .NonBlockingStream import InputStream, OutputStream, AppendStream, InputTer
 #import .System
 from .Timer import Timer
 from .UI import UI
-from .Various import StrToType, TypeDescription
+from .Various import StrToBool, StrToInt, StrToType, TypeDescription
 from .WarpxDataParser import WarpxDataParser
 from .Wrapper import Wrapper, SourceType
 from .LimitedBlockWriter import LimitedBlockWriter
